06e56d540a33b3acfd612bd96fcd9550b81f9be5
amanyadav2030 <amanyadav919821@gmail.com>
2022-12-15 10:14:19 +0530

    done

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const dbConnect = require('./config/db')
 const {UserRouter,EmiRouter} = require('./routes/index');
 const cors = require('cors');
+const PORT = process.env.PORT || 8080
 const app = express()
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
@@ -10,7 +11,7 @@ app.use('/user',UserRouter);
 app.use('/calculateEMI',EmiRouter);
 app.get('/', (req, res) => res.send('hello'))
 
-app.listen(8080, async() => {
+app.listen(PORT, async() => {
     await dbConnect();
     console.log('server started on port 8080')}
 )
