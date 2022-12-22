2c3134592af5d6a07271ebad54d4114b68cf6451
amanyadav2030 <amanyadav919821@gmail.com>
2022-12-22 13:54:07 +0530

    done

--- src/routes/question.routes.js
+++ src/routes/question.routes.js
@@ -3,8 +3,8 @@ const QuestionRouter = express.Router();
 const {Question} = require('../models/index.js')
 QuestionRouter.get('/',async(req,res)=>{
     try{
-        const {difficulty,category,limit=10} = req.body;
-        const questions = await Question.find({difficulty,category}).limit(limit);
+        const {difficulty='easy',category='Sports',limit=5} = req.query;
+        const questions = await Question.find({difficulty,category}).limit(limit)
         res.send(questions);
     }catch(e){
         return res.status(500).send({error:e.message});
